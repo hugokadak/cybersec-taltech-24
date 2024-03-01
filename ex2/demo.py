f885fe4a4a6b1f511fd0e8039b5accf0f39b3cda
Hugo <119073767+hugokadak@users.noreply.github.com>
2024-03-01 11:29:46 +0200

    mitte minu ;)

--- ex2/demo.py
+++ ex2/demo.py
@@ -1,25 +1,25 @@
 import time
 import paho.mqtt.client as paho
-broker="127.0.0.1"
+broker="10.8.0.1"
 
 # Define callback
 def on_message(client, userdata, message):
     time.sleep(1)
     print("received message =",str(message.payload.decode("utf-8")))
 
-client= paho.Client("client-001") #create client object client1.on_publish = on_publish #assign function to callback client1.connect(broker,port) #establish connection client1.publish("house/bulb1","on")
+client= paho.Client("client-3") #create client object client1.on_publish = on_publish #assign function to callback client1.connect(broker,port) #establish connection client1.publish("house/bulb1","on")
 
 # Bind function to callback
 client.on_message=on_message
 
 # Set username and password
-client.username_pw_set(username = "iot_module", password = "xxxxx")
+client.username_pw_set(username = "iot_module3", password = "parool")
 
 print("connecting to broker ",broker)
 client.connect(broker)   # connect
 client.loop_start()      # start loop to process received messages
 print("subscribing ")
-client.subscribe("class/iotNN") #subscribe
+client.subscribe("class/iot03") #subscribe
 time.sleep(2)
 
 try:

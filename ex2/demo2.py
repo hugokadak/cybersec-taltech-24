4aa18b24b0a61ea9d2c0053943375511e8e27619
Hugo <119073767+hugokadak@users.noreply.github.com>
2024-03-01 11:32:05 +0200

    minu oma 8)

---
+++ ex2/demo2.py
@@ -0,0 +1,32 @@
+import time
+import paho.mqtt.client as paho
+broker="10.8.0.1"
+
+# Define callback
+def on_message(client, userdata, message):
+    time.sleep(1)
+    print("received message =",str(message.payload.decode("utf-8")))
+
+client= paho.Client("client-8") #create client object client1.on_publish = on_publish #assign function to callback client1.connect(broker,port) #establish connection client1.publish("house/bulb1","on")
+
+# Bind function to callback
+client.on_message=on_message
+
+# Set username and password
+client.username_pw_set(username = "iot_module8", password = "parool")
+
+print("connecting to broker ",broker)
+client.connect(broker)   # connect
+client.loop_start()      # start loop to process received messages
+print("subscribing ")
+client.subscribe("class/iot08") #subscribe
+time.sleep(2)
+
+try:
+        while True:
+                    time.sleep(1)
+
+except KeyboardInterrupt:
+    print("exiting")
+    client.disconnect() #disconnect
+    client.loop_stop() #stop loop
